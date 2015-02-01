ed8c0a356f8e047c90cba5d711272f561a1a3c96
Gaurav Singh <singhgaurav@roomfarsouth-lm.barcelona.corp.yahoo.com>
2015-02-01 17:20:26 +0100

    prepare data

---
+++ prepare_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import random 
+
+data=pd.read_csv('fMRI_data/data-science-P1.csv')
+
+
+def select_2_classes_to_test():
+	global data
+	classes=pd.unique(data['class'].values)
+	test_classes=random.sample(classes, 2)
+	return test_classes
+
+def prepare_train_data(test_classes):
+	global data
+	idx=data['class'].isin(test_classes)
+	train_data=data[~idx]
+	return train_data
+
+def prepare_test_data(test_classes):
+	global data
+	idx=data['class'].isin(test_classes)
+	test_data=data[idx]
+	return test_data
+
+
+
+def train_model(train_data):	
+	model=Model()
+	model.train(train_data)
+	return model
+
+def evaluate_model(model, test_data):
+
+
+
+def main():
+	test_classes=select_2_classes_to_test()
+	train_data=prepare_train_data(test_classes)
+	test_data=prepare_test_data(test_classes)
+	model=train_model(train_data)
+	results=evaluate_model(model, test_data)
+	print results
+
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,28 +1,46 @@
 import scipy.io 
 import pickle
 import csv
-
+import numpy as np
 #global variables
-filename=""
+input_filename="fMRI_data/data-science-P1.mat"
+output_filename="fMRI_data/data-science-P1.csv"
+
+def get_data_from_mat_file(filename):
+	mat_data=scipy.io.loadmat(filename)
+	for i in xrange(360):
+		mat_temp=np.asmatrix(mat_data['data'][i][0][0])
+		if i==0:
+			mat=mat_temp
+		else:
+			mat=np.vstack((mat,mat_temp))
+
+	return mat
 
 
-get_data_from_mat_file(filename):
-	mat=scipy.io.loadmat(filename)
-	mat=mat['data'][0]
+
+def get_labels_from_mat_file(filename):
+	m2=[]
+	mat_data=scipy.io.loadmat(filename)
+	for i in xrange(360):
+		m2.append([mat_data['info'][0][i][2][0]])
+	return m2
 
 
 
 
-get_labels_from_mat_file(filename)
+m1=get_data_from_mat_file(input_filename)
+m2=get_labels_from_mat_file(input_filename)
 
 
-
-
-csvfile=open(filename, 'r')
+csvfile=open(output_filename, 'w')
+header=["c"+str(i) for i in xrange(len(list(np.asarray(m1)[0])))]+["class"]
 csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-for i in range(mat.shape[0]):
-	csvwriter.writerow(mat[i])
+csvwriter.writerow(header)
 
+for i in xrange(360):
+	data_to_write=list(np.asarray(m1)[i])+m2[i]
+	csvwriter.writerow(data_to_write)
 
 csvfile.close()
 
